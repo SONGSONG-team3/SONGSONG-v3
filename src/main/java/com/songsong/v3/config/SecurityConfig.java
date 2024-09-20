25c50e329187dc26c144e005e3855b522f9ae007
fbgjung <fbgjung@gmail.com>
2024-09-20 10:18:24 +0900

    feat: JWT 인증 및 보안 설정 추가
    - 세션 상태를 STATELESS로 설정하여 JWT 인증 처리
    
    - JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 앞에 추가

--- src/main/java/com/songsong/v3/config/SecurityConfig.java
+++ src/main/java/com/songsong/v3/config/SecurityConfig.java
@@ -1,15 +1,29 @@
 package com.songsong.v3.config;
 
+import com.songsong.v3.user.service.CustomUserDetailsService;
+import com.songsong.v3.common.JwtAuthenticationFilter;
+import com.songsong.v3.common.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CustomUserDetailsService customUserDetailsService;
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
@@ -20,11 +34,9 @@ public class SecurityConfig {
 
         http
                 .httpBasic(httpBasic -> httpBasic.disable())
-//                .formLogin(formLogin -> formLogin.disable())
-                .csrf(csrf -> csrf.disable());
+                .formLogin(formLogin -> formLogin.disable())
+                .csrf(csrf -> csrf.disable())
 
-
-        http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers(
                                 "/",
@@ -35,9 +47,20 @@ public class SecurityConfig {
                                 "/api/v3/users/**"
                                 ).permitAll()
 
-                        .anyRequest().authenticated());
+                        .anyRequest().authenticated())
+
+                // JWT를 통한 인증/인가를 위해서 세션을 STATELESS 상태로 설정
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, customUserDetailsService), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
 
